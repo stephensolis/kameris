0c91a1de6ec28686ba0d7be387f1c105fb3bfff3
Stephen <stephen@nowhere>
2017-07-05 15:51:28 -0400

    Fixed some bugs in selection

--- client/selection.py
+++ client/selection.py
@@ -17,8 +17,7 @@ from . import utils
 
 
 def run_selection(options, exp_options):
-    # because key 'dataset' (at least) may be added to a group's options
-    groups = copy.deepcopy(exp_options['groups'])
+    groups = exp_options['groups'].copy()
 
     # load selection functions
     selection_funcs = {}
@@ -32,26 +31,37 @@ def run_selection(options, exp_options):
 
     # run pick_group
     with utils.log_step('running pick_group'):
-        selected_metadata = []
+        pick_group_metadata = []
+        all_metadata_cache = {}
+
         for group_name, group_options in iteritems(groups):
+            group_options = group_options.copy()
+            groups[group_name] = group_options
+
             group_options['name'] = group_name
             if 'dataset' not in group_options:
                 group_options['dataset'] = exp_options['dataset']
 
-            # fetch group metadata
+            # fetch metadata
             all_metadata_filename = os.path.join(
                 options['metadata_dir'],
                 'metadata-{}.json'.format(group_options['dataset']['metadata'])
             )
-            with open(all_metadata_filename, 'r') as infile:
-                all_metadata = json.load(infile)
+            if all_metadata_filename in all_metadata_cache:
+                all_metadata = all_metadata_cache[all_metadata_filename]
+            else:
+                with open(all_metadata_filename, 'r') as infile:
+                    all_metadata = json.load(infile)
+                all_metadata_cache[all_metadata_filename] = all_metadata
 
             # perform selection
-            group_metadata = selection_funcs['pick_group_fn'](all_metadata,
-                                                              group_options)
+            group_metadata = selection_funcs['pick_group_fn'](
+                copy.deepcopy(all_metadata),
+                copy.deepcopy(group_options)
+            )
             for metadata_entry in group_metadata:
                 metadata_entry['group'] = group_name
-                selected_metadata.append(metadata_entry)
+                pick_group_metadata.append(metadata_entry)
 
     utils.mkdir_p(options['fasta_output_dir'])
 
@@ -61,7 +71,7 @@ def run_selection(options, exp_options):
         archive_cache = {}
         file_counter = 1
 
-        for metadata_entry in tqdm(selected_metadata, mininterval=1,
+        for metadata_entry in tqdm(pick_group_metadata, mininterval=1,
                                    file=utils.LoggerFileAdapter('modmap')):
             group_options = groups[metadata_entry['group']]
 
@@ -86,23 +96,29 @@ def run_selection(options, exp_options):
                         group_options['dataset']['archive_folder'], filename
                     )
 
+            # read file
             sequence_file = archive.open(filename)
             file_sequences = file_formats.read_fasta(sequence_file)
 
+            # run postprocess if required
             if 'postprocess' in options:
                 new_metadata, sequences_list = zip(
-                    *selection_funcs['postprocess_fn'](metadata_entry,
-                                                       file_sequences)
+                    *selection_funcs['postprocess_fn'](
+                        copy.deepcopy(metadata_entry), file_sequences
+                    )
                 )
             else:
                 new_metadata = [metadata_entry]
                 sequences_list = [file_sequences]
             final_metadata.extend(new_metadata)
 
-            for sequences, mt in zip(sequences_list, new_metadata):
+            # write fasta files
+            for sequences, final_metadata_entry in zip(sequences_list,
+                                                       new_metadata):
                 filename = str(file_counter).zfill(10) + '.fasta'
                 file_path = os.path.join(options['fasta_output_dir'], filename)
-                mt['filename'] = filename
+
+                final_metadata_entry['filename'] = filename
                 file_formats.export_fasta(file_path, sequences)
                 file_counter += 1
 
